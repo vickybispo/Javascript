89f1fa2d3ea84b1ac50adf5b73d09342928eb0c4
vickybispo <bisponatalysilva@gmail.com>
2024-06-28 15:46:27 -0300

    ex010

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+// console.log('O console funcionou corretamente')
+
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { // Condição Simples
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log('Dirija sempre com o cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Vivendo em ${País}`)
+if (país != 'Brasil') {
+    console.log(`Voce é Estrangeiro!`)
+} else {
+    console.log('Voce é Brasileiro!')
+}
