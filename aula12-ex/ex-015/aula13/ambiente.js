e1b8eee842576d139c729de0b88e9496cc5395e6
vickybispo <bisponatalysilva@gmail.com>
2024-08-25 12:15:24 -0300

    estudos de variaveis while, do e for

---
+++ aula12-ex/ex-015/aula13/ambiente.js
@@ -0,0 +1,12 @@
+var c = 1
+while (c <=30) {
+    console.log('Olá, bom dia! Tudo bem?')
+    c++
+} // Condiçao while 
+
+var c = 1
+do {
+    console.log('Olá, bom dia! Tudo bem?')
+    c++
+} while (c <=30)
+ // condiçao do
---
+++ aula13/ambiente.js
@@ -0,0 +1,5 @@
+var c = 1
+while (c <=30) {
+    console.log('Olá, bom dia! Tudo bem?')
+    c++
+}
