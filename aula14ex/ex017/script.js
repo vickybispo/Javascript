2c335898bd0bc81c88fdb32df4259928818cc659
vickybispo <bisponatalysilva@gmail.com>
2024-08-28 18:02:15 -0300

    resolucao dos ex 16 e ex17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('Seja Bem-Vindo(a)!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` // faz sentido em php, no js nem tanto
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
